3c51723fbf936324c59146e386da3111580b93e4
Mauro Tambillo <mauro.tambillo@gmail.com>
2025-02-14 15:39:40 -0500

    Login y Registro

--- Frontend/taskonnect/src/app/app.routes.ts
+++ Frontend/taskonnect/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import LoginComponent from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // { path: 'main', component: MainCalendarComponent, children:[
+  //     { path: 'editarperfil', title:"Editar perfil", component: EditarperfilComponent },
+  //     { path: 'calendar', title:"Calendar", component: CalendarComponent },
+  //     { path: 'editartarea', title:"Editar tarea", component: EditarTareaComponent, canActivate:[editTaskGuard] },
+  //     { path: 'creartarea', title:"Crear tarea", component: CrearTareaComponent },
+  //     { path: '', redirectTo: "/main/calendar", pathMatch: 'full'},
+  //   ],
+  //   canActivate:[loginGuard]
+  // },
+  // {path:'editarperfil',component:EditarperfilComponent},
+];
---
+++ Frontend/taskonnect/src/app/components/login/login.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import {Router, RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export default class LoginComponent {
+
+  usernameValue: string = 'ejemplo1@correo.com';
+  userPassword: string = 'contraUsuario1';
+
+  constructor(
+    private router: Router,
+    // private userService: UserService
+  ) { }
+
+  redirectToMain() {
+    this.router.navigate(['/main']);
+  }
+
+  mostrar: boolean = false; // password
+
+  errorMessage = {
+    count: 0,
+    email: {
+      text: "",
+      mostrar: false
+    },
+    password: {
+      text: "",
+      mostrar: false
+    },
+  }
+
+  successMessage = {
+    text: "",
+    mostrar: false,
+  }
+
+  loginErrorMessage = {
+    text: "",
+    mostrar: false,
+  }
+
+  public user = {
+    email: "",
+    password: "",
+  }
+
+  mostrarPass(){
+    this.mostrar=true;
+    let elemento:any = document.querySelector("[name='password']");
+    elemento.type = "text";
+  }
+
+  noMostrarPass(){
+    this.mostrar=false;
+    let elemento:any = document.querySelector("[name='password']");
+    elemento.type = "password";
+  }
+
+  async loginUsuario() {
+    console.log("Login Click")
+  }
+}
